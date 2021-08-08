ac61fab4a6fcd227414ab3296af0220eb7274c9b
Denis Djordjevic <betrocker@yahoo.com>
2021-08-08 15:46:52 +0200

    first commit

---
+++ components/footer/index.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div>
+      <h2>Ovo je footer</h2>
+    </div>
+  )
+}
---
+++ pages/cenovnik/index.js
@@ -0,0 +1,9 @@
+import Page from '../../components/Page'
+
+export default function cenovnik() {
+  return (
+    <Page title='Yabooka | Cenovnik' description='Cenovnik nasih usluga'>
+      <h2>Cenovnik</h2>
+    </Page>
+  )
+}
